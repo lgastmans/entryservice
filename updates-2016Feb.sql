5a7f2a2f19dd2dbd04ffd33f25edf2bac5a714b3
= <=>
2016-02-19 13:08:10 +0530

    SQL updates

--- updates-2016Feb.sql
+++ updates-2016Feb.sql
@@ -1,4 +1,23 @@
 ALTER TABLE `status` CHANGE `DurationPeriod` `DurationPeriod` ENUM( 'None', 'Days', 'Weeks', 'Months', 'Years' ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ;
 ALTER TABLE `applicant_status` CHANGE `DurationPeriod` `DurationPeriod` ENUM( 'None', 'Days', 'Weeks', 'Months', 'Years' ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ;
 
+===
+
 ALTER TABLE `applicant` CHANGE `MaritalStatus` `MaritalStatus` ENUM( 'Single', 'Partner', 'Married', 'Couple' ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ;
+
+===
+
+ALTER TABLE `children` ADD INDEX ( `SchoolID` ) ;
+ALTER TABLE `children` ADD FOREIGN KEY ( `SchoolID` ) REFERENCES `entryservice`.`school` (
+`ID`
+) ON DELETE RESTRICT ON UPDATE RESTRICT ;
+ALTER TABLE `children` ADD `Sex` ENUM( 'M', 'F' ) NOT NULL ;
+ALTER TABLE `children` ADD `NationalityID` INT NULL ,
+ADD INDEX ( `NationalityID` ) ;
+ALTER TABLE `children` ADD FOREIGN KEY ( `NationalityID` ) REFERENCES `entryservice`.`nationality` (
+`ID`
+) ON DELETE RESTRICT ON UPDATE RESTRICT ;
+ALTER TABLE `children` ADD `ResServiceNum` INT NULL ;
+
+===
+
