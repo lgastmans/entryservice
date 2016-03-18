d9fa3815db16a02a500de48d345b0774e465ef75
= <=>
2016-03-18 21:01:56 +0530

    remove h1 headers from absence create and update form

--- protected/views/absence/create.php
+++ protected/views/absence/create.php
@@ -15,6 +15,6 @@ $this->menu=array(
 );
 ?>
 
-<h1>Create Absence</h1>
+
 
 <?php $this->renderPartial('_form', array('model'=>$model)); ?>
--- protected/views/absence/update.php
+++ protected/views/absence/update.php
@@ -18,6 +18,6 @@ $this->menu=array(
 );
 ?>
 
-    <h1>Update Absence <?php echo $model->ID; ?></h1>
+    
 
 <?php $this->renderPartial('_form', array('model'=>$model)); ?>
